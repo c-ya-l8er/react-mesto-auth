c4e327affdb5a93c24194e3a3818eb72af96bc2a
c-ya-l8er <c-ya-l8er@ya.ru>
2023-09-17 13:00:57 +0300

    ПР 12 fix: исправил проблему деплоя

--- src/index.jsx
+++ src/index.jsx
@@ -7,7 +7,7 @@ import App from "./components/App.jsx";
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <BrowserRouter basename="/mesto-react-auth/">
+    <BrowserRouter basename="/">
       <App />
     </BrowserRouter>
   </React.StrictMode>
